1213c4ea235d20a26c4124ca907065680f6ec74a
Eleshinnla Tomisin <txmisxn1@gmail.com>
2024-10-06 19:04:03 -0500

    Tomisin implemented luhn and testing

--- 1015-WWCCV.py
+++ 1015-WWCCV.py
@@ -20,3 +20,25 @@ if len(card_number) == 16 and card_number.isdigit():
     print ("Card is valid.")
 else:
     print ("Invalid card number. It must be 16 digits long.") 
+
+def is_credit_card_valid(card_number):
+    # Assuming Luhn algorithm implementation exists here
+    def digits_of(n):
+        return [int(d) for d in str(n)]
+    digits = digits_of(card_number)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
+    checksum = sum(odd_digits)
+    for d in even_digits:
+        checksum += sum(digits_of(d * 2))
+    return checksum % 10 == 0
+
+def run_tests():
+    assert is_credit_card_valid("4111111111111111"), '4111111111111111 should pass but did not'
+    assert is_credit_card_valid("5105105105105100"), '5105105105105100 should pass but did not'
+    assert not is_credit_card_valid("134"), '134 should not pass but did'
+    assert not is_credit_card_valid("1234567890123456"), '1234567890123456 should not pass but did'
+    assert not is_credit_card_valid("000000000000"), 'This is a bad test and we will get an error message'
+
+run_tests()
+
